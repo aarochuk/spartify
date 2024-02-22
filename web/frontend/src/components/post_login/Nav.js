40f090c03f14db49269d759fc66cb55b7265eda9
uglkjgj <andrewarochukwu@gmail.com>
2024-02-22 11:29:06 -0500

    half way done with the song display

--- web/frontend/src/components/post_login/Nav.js
+++ web/frontend/src/components/post_login/Nav.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./Nav.css";
 import home from "../../images/nav-buttons/home.png";
 import home_active from "../../images/nav-buttons/home-active.png";
@@ -10,17 +11,35 @@ import temp_img from "../../images/temp-img.png";
 import github from "../../images/github.png";
 import github_active from "../../images/github-active.png";
 
-export default function Nav() {
+export default function Nav(props) {
+  const [gh, setGh] = useState(github);
+  const [navButtons, setNavButtons] = useState([home_active, party, music]);
   return (
     <div className="nav-bar">
-      <div className="user-img"><img src={temp_img} alt="" width="50%"/></div>
+      <div className="user-img">
+        <img src={temp_img} alt="" width="50%" />
+      </div>
       <div className="nav-buttons">
-        <button className="active"><img src={home_active} alt=""/></button>
-        <button><img src={party} alt=""/></button>
-        <button><img src={music} alt=""/></button>
+        <button className="active">
+          <img src={navButtons[0]} alt="" />
+        </button>
+        <button>
+          <img src={navButtons[1]} alt="" />
+        </button>
+        <button>
+          <img src={navButtons[2]} alt="" />
+        </button>
       </div>
       <div className="github-holder">
-        <img src={github}/>
+        <a
+          href="https://github.com/aarochuk"
+          target="_blank"
+          rel="noreferrer"
+          onMouseEnter={() => setGh(github_active)}
+          onMouseLeave={() => setGh(github)}
+        >
+          <img src={gh} />
+        </a>
       </div>
     </div>
   );
--- web/frontend/src/pages/Home.js
+++ web/frontend/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Home.css";
 import Nav from "../components/post_login/Nav";
+import sound from "../images/sound-waves.png";
+import lit from "../images/litbonus.png";
+import queue from "../images/playlist.png";
+import essence from "../images/essence.png";
+import passionfruit from "../images/passionfruit.png";
 
 export default function Home() {
   return (
     <div className="main">
-      <Nav/>
+      <Nav active="home" />
       <div className="main-content">
         <div className="main-top">
           <button>Search</button>
@@ -46,21 +51,48 @@ export default function Home() {
         </div>
       </div>
       <div className="playing-song">
-        <p>Now Playing</p>
-        <img />
-        <p>name</p>
-        <p>artist</p>
-        <div>progress bar</div>
-        <div>
-          <img />
-          <img />
-          <img />
+        <div className="top">
+          <img src={sound} alt="" />
+          <p>Now Playing</p>
         </div>
-        <div>
-          <p>Queue</p>
-          <div>song</div>
-          <div>song</div>
-          <div>song</div>
+        <div className="img-holder">
+          <img src={lit} width="90%" style={{ borderRadius: "5%" }} alt="" />
+        </div>
+        <div className="song-details">
+          <div>
+            <p>Lit - Bonus</p>
+            <p>Bas, J.Cole, KQuick</p>
+          </div>
+          <div>
+            <img src={queue} alt="" />
+          </div>
+        </div>
+        <div>progress bar</div>
+        <div style={{ overflow: "auto" }}>
+          <div>
+            <img
+              src={essence}
+              width="15%"
+              style={{ borderRadius: "10%" }}
+              alt=""
+            />
+            <div>
+              <p>Essence</p>
+              <p>Wizkid</p>
+            </div>
+          </div>
+          <div>
+            <img
+              src={passionfruit}
+              width="15%"
+              style={{ borderRadius: "10%" }}
+              alt=""
+            />
+            <div>
+              <p>Passionfruit</p>
+              <p>Drake</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
