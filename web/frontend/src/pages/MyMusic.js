8c2e600ebbd4211bad32fd799ae59775171a0483
uglkjgj <andrewarochukwu@gmail.com>
2024-02-22 19:05:13 -0500

    finished basic design of my music page

--- web/frontend/src/pages/MyMusic.js
+++ web/frontend/src/pages/MyMusic.js
@@ -5,14 +5,29 @@ import SongDetails from "../components/post_login/SongDetails";
 import Track from "../components/post_login/Track";
 import essence from "../images/essence.png";
 import passionfruit from "../images/passionfruit.png";
+import shuffle from "../images/arrow.png";
+import play from "../images/play-button.png";
+import list from "../images/list.png";
+import small_search from "../images/small-search.png";
 
 export default function MyMusic() {
   return (
     <div className="main">
       <Nav active="mymusic" />
       <div className="main-content">
-        <div>
-          <p>Join Party</p>
+        <p className="ms">My Songs</p>
+        <div className="header-holder">
+          <div className="players">
+            <img src={play} alt="" />
+            <img src={shuffle} alt="" />
+          </div>
+          <div className="searchers">
+            <img src={small_search} alt="" />
+            <div className="organizers">
+              <p>Date Added</p>
+              <img src={list} alt="" />
+            </div>
+          </div>
         </div>
         <div className="track-holder">
           <Track
